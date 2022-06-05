4cf3e29967a6981ec33564b0abfb4edf4b568894
Naman1997 <aroranaman17@gmail.com>
2022-06-05 13:47:55 +0530

    Fix scheme for serializer

--- cmd/kube-snap/main.go
+++ cmd/kube-snap/main.go
@@ -3,61 +3,110 @@ package main
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 
-	// "os"
+	"os"
 	// "path/filepath"
 
+	appsv1 "github.com/openshift/api/apps/v1"
+	authorizationv1 "github.com/openshift/api/authorization/v1"
+	buildv1 "github.com/openshift/api/build/v1"
+	imagev1 "github.com/openshift/api/image/v1"
+	networkv1 "github.com/openshift/api/network/v1"
+	oauthv1 "github.com/openshift/api/oauth/v1"
+	projectv1 "github.com/openshift/api/project/v1"
+	quotav1 "github.com/openshift/api/quota/v1"
+	routev1 "github.com/openshift/api/route/v1"
+	securityv1 "github.com/openshift/api/security/v1"
+	templatev1 "github.com/openshift/api/template/v1"
+	userv1 "github.com/openshift/api/user/v1"
+	v1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	jsonserializer "k8s.io/apimachinery/pkg/runtime/serializer/json"
-	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer/json"
 
-	// git "github.com/go-git/go-git/v5"
-	// "github.com/go-git/go-git/v5/plumbing/transport/http"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer/versioning"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd/api"
 )
+
+const Version = "v1"
+
+var Codec runtime.Codec
+
+func init() {
+	appsv1.AddToScheme(scheme.Scheme)
+	authorizationv1.AddToScheme(scheme.Scheme)
+	buildv1.AddToScheme(scheme.Scheme)
+	imagev1.AddToScheme(scheme.Scheme)
+	networkv1.AddToScheme(scheme.Scheme)
+	oauthv1.AddToScheme(scheme.Scheme)
+	projectv1.AddToScheme(scheme.Scheme)
+	quotav1.AddToScheme(scheme.Scheme)
+	routev1.AddToScheme(scheme.Scheme)
+	securityv1.AddToScheme(scheme.Scheme)
+	templatev1.AddToScheme(scheme.Scheme)
+	userv1.AddToScheme(scheme.Scheme)
+	appsv1.AddToSchemeInCoreGroup(scheme.Scheme)
+	authorizationv1.AddToSchemeInCoreGroup(scheme.Scheme)
+	buildv1.AddToSchemeInCoreGroup(scheme.Scheme)
+	imagev1.AddToSchemeInCoreGroup(scheme.Scheme)
+	networkv1.AddToSchemeInCoreGroup(scheme.Scheme)
+	oauthv1.AddToSchemeInCoreGroup(scheme.Scheme)
+	projectv1.AddToSchemeInCoreGroup(scheme.Scheme)
+	quotav1.AddToSchemeInCoreGroup(scheme.Scheme)
+	routev1.AddToSchemeInCoreGroup(scheme.Scheme)
+	securityv1.AddToSchemeInCoreGroup(scheme.Scheme)
+	templatev1.AddToSchemeInCoreGroup(scheme.Scheme)
+	userv1.AddToSchemeInCoreGroup(scheme.Scheme)
+}
 
 func main() {
 
 	/*
 		TODO:
-		- Git clone a repo using creds from a scret
-		- Remove all non dot files and dirs in the cloned dir
 		- Write files in a specific directory structure
 		- Git push to the same specified branch
 	*/
 
-	// _, err := git.PlainClone("/repo/", false, &git.CloneOptions{
-	// 	Auth: &http.BasicAuth{
-	// 		Username: getValueOf("repo-user", "abc123"),
-	// 		Password: getValueOf("repo-pass", ""),
-	// 	},
-	// 	URL:        getValueOf("repo-url", ""),
-	// 	RemoteName: getValueOf("repo-branch", ""),
-	// 	Progress:   os.Stdout,
-	// })
-
-	// files, err := filepath.Glob("/repo/.*")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// for _, f := range files {
-	// 	if err := os.Remove(f); err != nil {
-	// 		panic(err)
-	// 	}
-	// }
-
-	// Serializer = Decoder + Encoder.
-	serializer := jsonserializer.NewSerializerWithOptions(
-		jsonserializer.DefaultMetaFactory,
-		scheme.Scheme,
-		scheme.Scheme,
-		jsonserializer.SerializerOptions{
-			Yaml:   true,
-			Pretty: true,
-			Strict: false,
+	// Git clone a repo using creds from a secret
+	_, err := git.PlainClone("./repo/", false, &git.CloneOptions{
+		Auth: &http.BasicAuth{
+			Username: getValueOf("repo-user", "abc123"),
+			Password: getValueOf("repo-pass", ""),
 		},
+		URL:        getValueOf("repo-url", ""),
+		RemoteName: getValueOf("repo-branch", ""),
+		Progress:   os.Stdout,
+	})
+
+	// Remove all non dot files and dirs in the cloned dir
+	files, err := filepath.Glob("./repo/.*")
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range files {
+		if err := os.Remove(f); err != nil {
+			panic(err)
+		}
+	}
+
+	s := scheme.Scheme
+	utilruntime.Must(api.AddToScheme(s))
+	utilruntime.Must(v1.AddToScheme(s))
+	serializer := json.NewYAMLSerializer(json.DefaultMetaFactory, s, s)
+	Codec = versioning.NewDefaultingCodecForScheme(
+		s,
+		serializer,
+		serializer,
+		schema.GroupVersion{Version: Version},
+		runtime.InternalGroupVersioner,
 	)
 
 	// creates the in-cluster config
@@ -77,7 +126,7 @@ func main() {
 		panic(err.Error())
 	}
 	for node_index := range nodes.Items {
-		yaml, err := runtime.Encode(serializer, &nodes.Items[node_index])
+		yaml, err := runtime.Encode(Codec, &nodes.Items[node_index])
 		if err != nil {
 			panic(err.Error())
 		}
@@ -91,7 +140,7 @@ func main() {
 		panic(err.Error())
 	}
 	for node_index := range namespaces.Items {
-		yaml, err := runtime.Encode(serializer, &namespaces.Items[node_index])
+		yaml, err := runtime.Encode(Codec, &namespaces.Items[node_index])
 		if err != nil {
 			panic(err.Error())
 		}
--- cmd/kube-snap/util.go
+++ cmd/kube-snap/util.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 	"strings"
 )
@@ -13,4 +15,22 @@ func getValueOf(key, fallback string) string {
 	data := string(value)
 	data = strings.Trim(data, "\"")
 	return data
+}
+
+func createFile(path string, data string) {
+	f, err := os.Create(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	_, err2 := f.WriteString(data)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println("done")
 }
